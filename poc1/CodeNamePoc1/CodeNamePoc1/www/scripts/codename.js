03eefcfc14e06456e11a84eb754a343e519b48c5
Vincent Carluer <vcarluer@gmail.com>
2015-09-17 10:56:45 +0200

    poc1

---
+++ poc1/CodeNamePoc1/CodeNamePoc1/www/scripts/codename.js
@@ -0,0 +1,11 @@
+﻿var Codename = function () { };
+
+(function () {
+	"use strict";
+
+	Codename.prototype.run = function () {
+		this.div = document.getElementById("codename");
+		this.div.innerHTML = "Hello codename";
+	};
+
+}());
