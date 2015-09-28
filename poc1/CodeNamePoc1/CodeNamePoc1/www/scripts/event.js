1dfb5dea209c7704dd27a1320d1bb464398628cb
vcarluer <vcarluer@gmail.com>
2015-09-28 23:42:01 +0200

    first event file structure research

---
+++ poc1/CodeNamePoc1/CodeNamePoc1/www/scripts/event.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict"
+
+    var Event = function () { };
+    Codename.Event = Event;
+
+    Event.prototype.create = function (episode, eventName) {
+        var self = this;
+        this.eventPath = episode.episodePath;
+        this.episode = episode;
+        Codename.loadGameObject(this.dialogPath, eventName, function (event) {
+            self.event = event;
+            event.event = self;
+        });
+    };
+
+}());
---
+++ poc1/CodeNamePoc1/CodeNamePoc1/www/scripts/eventGroup.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    "use strict"
+
+    var EventGroup = function () { };
+    EventGroup.prototype = Object.create(Codename.Event);
+
+    Codename.EventGroup = EventGroup;
+
+    EventGroup.init = function () {
+        this.events = [];
+    };
+
+}());
